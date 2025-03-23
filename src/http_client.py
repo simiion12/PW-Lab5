71c989f72726da8d619e9eaa54f51bfda6b591ee
russian17 <simion.cuzmin@isa.utm.md>
2025-03-23 14:52:39 +0200

    added prepare_headers method

--- src/http_client.py
+++ src/http_client.py
@@ -4,6 +4,7 @@ class HttpClient:
     def make_http_request(self, url, method='GET', accept=None, data=None, headers=None, follow_redirects=True, max_redirects=5):
 
         parsed_url = self._parse_url(url)
+        request_headers = self._prepare_headers(parsed_url, headers, accept)
 
 
     def _parse_url(self, url):
@@ -12,4 +13,14 @@ class HttpClient:
         if not parsed_url.path:
             parsed_url = parsed_url._replace(path='/')
         return parsed_url
+
+    def _prepare_headers(self, parsed_url, headers, accept):
+        """Prepare headers for http request."""
+        hostname = parsed_url.netloc
+        headers['Host'] = hostname.split(':')[0]
+        headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+        headers['Connection'] = "close"
+        if accept:
+            headers['Accept'] = accept
+        return headers
 
